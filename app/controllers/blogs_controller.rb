34d48d76073bfc19116340c1e76bf1206bd44243
shuto kikai <yysskikai04@icloud.com>
2023-07-07 21:00:18 +0900

    commit

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,7 @@
 class BlogsController < ApplicationController
+
   def index
+    @blogs = Blog.all #Blogの中にあるすべてのレコードを出力します
   end
 
   def new
@@ -7,11 +9,28 @@ class BlogsController < ApplicationController
   end
 
   def create 
-    Blog.create(params. require(:blog).permit(:title, :content))
-    redirect_to new_blog_path
-  end_params
-  params.require(:blog).permit(:title, :content)
+    # Blog.create(params. require(:blog).permit(:title, :content))
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to new_blog_path, notice: "ブログを作成しました！"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+  
 end
+
+
 
 
 
