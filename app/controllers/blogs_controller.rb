0d48e1d5bf2e67794f0cfc89dedf314c60f5049e
shuto kikai <yysskikai04@icloud.com>
2023-07-08 20:58:39 +0900

    commit

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,9 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   def index
     @blogs = Blog.all #Blogの中にあるすべてのレコードを出力します
-    binding.pry
+    
   end
 
   def new
@@ -20,7 +21,33 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.find(params[:id])
+    @blog 
+    # = Blog.find(params[:id])
+  end
+
+  def edit
+    @blog 
+    # = Blog.find(params[:id])
+  end
+
+  def update
+    @blog 
+    # = Blog.find(params[:id])
+
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: "ブログを削除しました!"
+  end
+
+  def confirm
+    @blog = Blog.new(blog_params)
   end
 
   private
@@ -29,6 +56,10 @@ class BlogsController < ApplicationController
     params.require(:blog).permit(:title, :content)
   end
   
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@
 #   get '/stocks', to: 'stocks#index'
 # end
 Rails.application.routes.draw do
-  resources :blogs
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
 end
